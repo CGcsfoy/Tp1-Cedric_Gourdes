ea7b017ac216a8fe531e8af789ef90266cd53cfd
CedricGourdes <2346909@csfoy.ca>
2025-03-05 08:32:58 -0500

    Validation des controlleurs
    Ajout de la validation dans les controlleurs qui en avait pas encore

--- Tp1-ServiceWeb/app/Http/Controllers/ActorController.php
+++ Tp1-ServiceWeb/app/Http/Controllers/ActorController.php
@@ -11,7 +11,19 @@ class ActorController extends Controller
     // Route 2
     public function index($id)
     {
-        $film = Film::findOrFail($id);
-        return ActorResource::collection($film->actors);
+        try {
+            $film = Film::findOrFail($id);
+            return ActorResource::collection($film->actors);
+        } catch (QueryException $e) {
+            return response()->json([
+                'error' => 'Erreur base de données',
+                'message' => $e->getMessage(),
+            ], HttpStatusCodes::INTERNAL_SERVER_ERROR);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Film non trouvé',
+                'message' => $e->getMessage(),
+            ], HttpStatusCodes::NOT_FOUND);
+        }
     }
 }
